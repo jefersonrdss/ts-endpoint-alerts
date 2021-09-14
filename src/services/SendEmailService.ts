9c6a934bc036c88d440629d89f6afb886344fa8f
Jeferson Rodrigues <jefersonr.santos@outlook.com>
2021-09-14 17:26:25 -0300

    fineshed SendEmailService

---
+++ src/services/SendEmailService.ts
@@ -0,0 +1,73 @@
+/**
+ * Author: Jeferson Rodrigues <jefersonr.santos@outlook.com>
+ * Created at: 2021-09-14
+ * Updated at: 2021-09-14
+ */
+
+import 'dotenv/config';
+import nodemailer from "nodemailer"
+
+interface IDataEmail {
+    subject: string;
+    message: string;
+}
+
+class SendEmailService {
+
+    private smtp_server: string | undefined;
+    private smtp_port: string | undefined;
+    private smtp_user: string | undefined;
+    private smtp_pass: string | undefined;
+    private source: string | undefined;
+    private subject: string;
+    private message: string;
+
+    // construtor
+    constructor({ subject, message }: IDataEmail) {
+
+        // smtp config
+        this.smtp_server = process.env.SMTP_SERVER;
+        this.smtp_port = process.env.SMTP_PORT;
+        this.smtp_user = process.env.SMTP_USER;
+        this.smtp_pass = process.env.SMTP_PASS;
+;
+        // email config
+        this.source = process.env.SMTP_FROM;
+        this.subject = subject;
+        this.message = message;
+    }
+
+    // Metodo para realizar o envio do email
+    async executeSendEmail(destination: string): Promise<void> {
+
+        if(this.smtp_server === undefined || this.smtp_port === undefined) {
+            throw new Error("Incomplete configuration. SMTP SERVER or PORT is missing on config file")
+        }
+
+        // configs do smtp server
+        const transport = nodemailer.createTransport({
+            host: this.smtp_server,
+            port: parseInt(this.smtp_port),
+            secure: false,
+            auth: { // auth
+                user: this.smtp_user,
+                pass: this.smtp_pass
+            },
+            tls: {
+                // desabilita erros para certificado invalido
+                rejectUnauthorized: false,
+                ciphers: 'TLSv1'
+            },
+        });
+
+        // enviar o email
+        await transport.sendMail({
+            from: this.source,
+            to: destination,
+            subject: this.subject,
+            html: this.message
+        });
+    }
+}
+
+export { SendEmailService }
